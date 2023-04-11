3c734cf85ae550404e8aa8534e9a1ab7e111b8b4
Abyl239 <altajanovabylay@gmail.com>
2023-04-11 16:09:43 +0600

    icon

--- src/components/step-process/index.jsx
+++ src/components/step-process/index.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from "react";
 import "./index.css";
+import TopIcon from "../../assets/icontop-removebg-preview.png"
 
 function StepProcess() {
     useEffect(() => {
@@ -7,10 +8,11 @@ function StepProcess() {
         const line = document.querySelector(".Thing-dash");
         const lineDefault = document.querySelector(".Thing-dash-default");
         const content = document.querySelectorAll(".Thing-content")
+        const pointer = document.querySelectorAll(".Thing-pointer")
         if(window.innerWidth<651){
             content[0].style.display="block"
         }
-
+        pointer[0].style.opacity="1"
 
         lineDefault.style.width=`${steps[3].parentNode.offsetLeft + steps[3].offsetWidth / 2}px`
         steps.forEach((step, index) => {
@@ -22,8 +24,10 @@ function StepProcess() {
                 line.style.backgroundColor = "red";
                 content.forEach((contn,ind)=>{
                     contn.style.display="none"
+                    pointer[ind].style.opacity="0"
                 })
                 const timer = setTimeout(() => {
+                    pointer[index].style.opacity="1"
                     if(index!==0) {
                         content[index].style.display = "block"
                     }
@@ -46,12 +50,12 @@ function StepProcess() {
                     <div className="Thing-dash-default"/>
                     <div className="Thing-dash"/>
                     <div className="Thing">
-                        <div className="Thing-info">
-                        </div>
+                        <div className="Thing-info"/>
                         <div className="Thing-step first"/>
                         <div className="Thing-info">
                             <h3>Ты здесь!</h3>
                         </div>
+                        <img className="Thing-pointer" width="20" src={TopIcon} alt=""/>
                         <div className="Thing-content">
                             <h3>Ты здесь!</h3>
                             <p>Доработка функции мобильного приложения </p>
@@ -68,12 +72,12 @@ function StepProcess() {
                         <div className="Thing-info">
                             <p>Доработка функции мобильного приложения</p>
                         </div>
+                        <img className="Thing-pointer" width="20" src={TopIcon} alt=""/>
                         <div className="Thing-content">
-                            <h3>Midterm exam №2</h3>
+                            <h3>Midterm exam №1</h3>
                             <p>Доработка функции мобильного приложения </p>
                         </div>
                     </div>
-
                     <div className="Thing">
                         <div className="Thing-info">
                             <h3>
@@ -83,14 +87,14 @@ function StepProcess() {
                         </div>
                         <div className="Thing-step"/>
                         <div className="Thing-info">
-                            <p>Разработка модуля приложения - торговой  системы </p>
+                            <p>Разработка модуля приложения - торговой системы </p>
                         </div>
+                        <img className="Thing-pointer" width="20" src={TopIcon} alt=""/>
                         <div className="Thing-content">
-                            <h3>Midterm exam №1</h3>
-                            <p>Доработка функции мобильного приложения </p>
+                            <h3>Midterm exam №2</h3>
+                            <p>Разработка модуля приложения - торговой системы </p>
                         </div>
                     </div>
-
                     <div className="Thing">
                         <div className="Thing-info last">
                             <h3>Final exam</h3>
@@ -99,9 +103,10 @@ function StepProcess() {
                         <div className="Thing-info">
                             <p>Разработка модуля приложения - торговой системы </p>
                         </div>
+                        <img className="Thing-pointer" width="20" src={TopIcon} alt=""/>
                         <div className="Thing-content">
-                            <h3>Midterm exam №1</h3>
-                            <p>Доработка функции мобильного приложения </p>
+                            <h3>Final exam</h3>
+                            <p>Разработка модуля приложения - торговой системы </p>
                         </div>
                     </div>
                 </div>
