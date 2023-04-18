be3eda7fff95c2803f920cd4f0692656c85db84b
Abyl239 <altajanovabylay@gmail.com>
2023-04-19 02:06:14 +0600

    fixed

--- src/components/lesson-teacher/index.jsx
+++ src/components/lesson-teacher/index.jsx
@@ -4,7 +4,7 @@ import "./index.css"
 const LessonTeacher = ({lessonTeacher}) => {
     return (
         <div className="lesson-teacher">
-            <h3 className="lesson-teacher__title">Наш предподаватель:</h3>
+            <h3 className="lesson-teacher__title">Наш преподаватель:</h3>
             <div className="lesson-teacher__content main-box">
                 <div className="lesson-teacher__img">
                     <img width="300"  src={lessonTeacher.image} alt=""/>
