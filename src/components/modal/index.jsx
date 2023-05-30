7fc366fc944a42f1646801986c60843589a1b9b2
Abyl239 <altajanovabylay@gmail.com>
2023-05-30 17:55:47 +0600

    amoForm

--- src/components/modal/index.jsx
+++ src/components/modal/index.jsx
@@ -1,24 +1,45 @@
-import React from 'react';
-import "./index.css"
-import SendForm from "../send-form";
-import {useTranslation} from "react-i18next";
+import React, { useEffect, useRef } from 'react';
+import './index.css';
+import { useTranslation } from 'react-i18next';
 
-const Modal = ({setModal}) => {
-    const {t} = useTranslation()
-    const closeModal = (event)=>{
-        event.preventDefault()
-        if(event.target.classList.contains("modal")){
-            setModal(false)
+const Modal = ({ setModal }) => {
+    const { t } = useTranslation();
+
+    const closeModal = (event) => {
+        event.preventDefault();
+        if (event.target.classList.contains('modal')) {
+            setModal(false);
         }
-    }
+    };
+
+    const formRef = useRef();
+
+    useEffect(() => {
+        const script = document.createElement('script');
+        script.innerHTML = `!function(a,m,o,c,r,m){a[o+c]=a[o+c]||{setMeta:function(p){this.params=(this.params||[]).concat([p])}},a[o+r]=a[o+r]||function(f){a[o+r].f=(a[o+r].f||[]).concat([f])},a[o+r]({id:"1144762",hash:"4e12781ec5203a2daa244c3adb2c69eb",locale:"ru"}),a[o+m]=a[o+m]||function(f,k){a[o+m].f=(a[o+m].f||[]).concat([[f,k]])}}(window,0,"amo_forms_","params","load","loaded");`;
+
+        const amoFormsScript = document.createElement('script');
+        amoFormsScript.id = 'amoforms_script_1144762';
+        amoFormsScript.async = true;
+        amoFormsScript.charset = 'utf-8';
+        amoFormsScript.src =
+            'https://forms.amocrm.ru/forms/assets/js/amoforms.js?1685444331';
+
+        formRef.current.appendChild(script);
+        formRef.current.appendChild(amoFormsScript);
+
+        return () => {
+        };
+    }, []);
+
     return (
         <div className="modal" onClick={closeModal}>
             <div className="modal__content animate">
                 <div className="modal__text">
-                    <h3>{t("modalH3")}</h3>
-                    <p>{t("modalText")}</p>
+                    <h3>{t('modalH3')}</h3>
+                    <p>{t('modalText')}</p>
                 </div>
-                <SendForm isEmail={false} message_type={'Записался на курс'}/>
+                <div className="modal__form" ref={formRef}></div>
             </div>
         </div>
     );
--- src/components/send-form/index.jsx
+++ src/components/send-form/index.jsx
@@ -1,55 +1,10 @@
-import React, {useRef, useState} from 'react';
-import emailjs from '@emailjs/browser';
-import "./index.css";
-import {useTranslation} from "react-i18next";
+import React, { useEffect, useRef } from 'react';
+import './index.css';
 
-const SendForm = ({ isEmail,message_type }) => {
-    const {t} = useTranslation()
-    const sendForm = useRef();
-    const message = useRef();
-    const [isSent, setIsSent] = useState(false)
+const SendForm = () => {
 
-    const [messageText,setMessageText] = useState('')
 
-    const sendEmail = (e) => {
-        e.preventDefault();
-        const form = {
-            user_name:sendForm.current.user_name.value,
-            user_phone_number:sendForm.current.user_phone_number.value,
-            message_type:message_type,
-            user_email:isEmail?sendForm.current.user_email.value:'',
-        }
-        if(form.user_name && form.user_phone_number) {
-            setMessageText(t("formMessage1"))
-            setIsSent(true)
-        }
-        else{
-            setMessageText(t("formMessage2"))
-            setIsSent(false)
-        }
-        // setMessageText(t("formMessage3"))
-        // setIsSent(false)
-
-    };
-
-    return (
-        <div className="send-form">
-            <form ref={sendForm} className="send-form__form">
-                <div className={isEmail?'row':'column'}>
-                    <input name="user_name" className="my-input" type="text" placeholder={t("formName")} />
-                    <input name="user_phone_number" className="my-input" type="tel" placeholder={t("formPhoneNumber")} />
-                </div>
-                {isEmail?
-                    <input name="user_email" className="my-input" type="email" placeholder={t("formEmail")} />
-                    :<></>
-                }
-                <button className="my-btn" onClick={sendEmail}>{message_type==="Получить консультацию"?t("formButton2"):t("formButton1")}</button>
-            </form>
-            <div ref={message} className={`send-form__message ${isSent?'green':'red'}`}>
-                {messageText}
-            </div>
-        </div>
-    );
+    return <div>{/* Your form content */}</div>;
 };
 
 export default SendForm;
