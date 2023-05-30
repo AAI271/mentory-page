ca3c47b9cc3154d503ad39e926b2bfa5c03372f2
Abyl239 <altajanovabylay@gmail.com>
2023-05-30 18:16:08 +0600

    amoForm

--- src/components/modal/index.jsx
+++ src/components/modal/index.jsx
@@ -4,6 +4,7 @@ import { useTranslation } from 'react-i18next';
 
 const Modal = ({ setModal }) => {
     const { t } = useTranslation();
+    let gh_page_problem = true;
 
     const closeModal = (event) => {
         event.preventDefault();
@@ -15,19 +16,20 @@ const Modal = ({ setModal }) => {
     const formRef = useRef();
 
     useEffect(() => {
-        const script = document.createElement('script');
-        script.innerHTML = `!function(a,m,o,c,r,m){a[o+c]=a[o+c]||{setMeta:function(p){this.params=(this.params||[]).concat([p])}},a[o+r]=a[o+r]||function(f){a[o+r].f=(a[o+r].f||[]).concat([f])},a[o+r]({id:"1144762",hash:"4e12781ec5203a2daa244c3adb2c69eb",locale:"ru"}),a[o+m]=a[o+m]||function(f,k){a[o+m].f=(a[o+m].f||[]).concat([[f,k]])}}(window,0,"amo_forms_","params","load","loaded");`;
+        if(gh_page_problem) {
+            const script = document.createElement('script');
+            script.innerHTML = `!function(a,m,o,c,r,m){a[o+c]=a[o+c]||{setMeta:function(p){this.params=(this.params||[]).concat([p])}},a[o+r]=a[o+r]||function(f){a[o+r].f=(a[o+r].f||[]).concat([f])},a[o+r]({id:"1144762",hash:"4e12781ec5203a2daa244c3adb2c69eb",locale:"ru"}),a[o+m]=a[o+m]||function(f,k){a[o+m].f=(a[o+m].f||[]).concat([[f,k]])}}(window,0,"amo_forms_","params","load","loaded");`;
 
-        const amoFormsScript = document.createElement('script');
-        amoFormsScript.id = 'amoforms_script_1144762';
-        amoFormsScript.async = true;
-        amoFormsScript.charset = 'utf-8';
-        amoFormsScript.src =
-            'https://forms.amocrm.ru/forms/assets/js/amoforms.js?1685444331';
+            const amoFormsScript = document.createElement('script');
+            amoFormsScript.id = 'amoforms_script_1144762';
+            amoFormsScript.async = true;
+            amoFormsScript.charset = 'utf-8';
+            amoFormsScript.src =
+                'https://forms.amocrm.ru/forms/assets/js/amoforms.js?1685444331';
 
-        formRef.current.appendChild(script);
-        formRef.current.appendChild(amoFormsScript);
-
+            formRef.current.appendChild(script);
+            formRef.current.appendChild(amoFormsScript);
+        }
         return () => {
         };
     }, []);
